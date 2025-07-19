47583acca5952f8f836f2741b316e1bc13d6f5b7
Mark <mhaynes031395@gmail.com>
2025-07-19 12:38:38 -0400

    initisl commit before logic

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+// console.log(document.querySelector('.message').textContent);
+
+// const newLocal = (document.querySelector('.message').textContent =
+//   'Correct 😁');
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ Nothing Guessed';
+  }
+});
